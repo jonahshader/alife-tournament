54d23dcd656021ddbfff1d720d8ef70b023c9e3a
Ruar-p <rpodushkin@oakland.edu>
2023-02-15 11:32:51 -0500

    Environment now handles spawning of creatures and food

--- core/src/main/java/com/csi4999/systems/environment/Environment.java
+++ core/src/main/java/com/csi4999/systems/environment/Environment.java
@@ -1,23 +1,33 @@
 package com.csi4999.systems.environment;
 
 import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.math.RandomXS128;
+import com.csi4999.systems.creature.Creature;
 import com.csi4999.systems.creature.SensorBuilder;
 import com.csi4999.systems.creature.ToolBuilder;
 import com.csi4999.systems.physics.PhysicsEngine;
 import space.earlygrey.shapedrawer.ShapeDrawer;
 
 import java.util.List;
+import java.util.Random;
 
 public class Environment {
+    // One and only instance of random
     PhysicsEngine physics;
+    Random r;
     // Environment will eventually have all sorts of spawners in it (food, creature, artifacts)
+
+    FoodSpawner foodSpawner;
+    CreatureSpawner creatureSpawner;
 
     List<SensorBuilder> availableSensors;
     List<ToolBuilder> availableTools;
 
-    public Environment() {
+    public Environment(int initialFood, int initalCreatures) {
         this.physics = new PhysicsEngine();
-
+        this.r = new RandomXS128();
+        this.foodSpawner = new FoodSpawner(initialFood, this.r, this.physics);
+        this.creatureSpawner = new CreatureSpawner(initalCreatures, this.r, this.physics);
     }
 
     public void drawObjects(ShapeDrawer drawer, Batch batch) {
@@ -25,7 +35,7 @@ public class Environment {
     }
 
     public void moveObjects(float dt) {
+        physics.run();
         physics.move(dt);
     }
-
 }
--- core/src/main/java/com/csi4999/systems/environment/Food.java
+++ core/src/main/java/com/csi4999/systems/environment/Food.java
@@ -10,16 +10,30 @@ import space.earlygrey.shapedrawer.ShapeDrawer;
 import java.util.List;
 
 public class Food extends Circle {
-
-
+    // Basically just a testball right now
+    public Food(Vector2 position, Vector2 velocity, Vector2 acceleration, float radius) {
+        super(position, velocity, acceleration, radius);
+    }
 
     @Override
     public void draw(Batch batch, ShapeDrawer shapeDrawer, float parentAlpha) {
-
+        shapeDrawer.setColor(color.r, color.g, color.b, parentAlpha);
+        shapeDrawer.filledCircle(0f, 0f, this.radius);
     }
 
     @Override
     public void receiveActiveColliders(List<Collider> activeColliders) {
-
+        if (activeColliders.size() > 0) {
+            radius -= 0.05f;
+            color.r = 1f;
+            color.g = 0f;
+        } else {
+            radius += 0.01f;
+            color.r = 0f;
+            color.g = 1f;
+        }
+        color.b -= 0.01f;
+        color.b = Math.max(color.b, 0f);
+        radius = Math.max(radius, 2f);
     }
 }
