43c395a6d1c42c3efab93f5fb5a81d3362084931
rfrost114 <82419789+rfrost114@users.noreply.github.com>
2023-02-21 15:06:52 -0500

    add saved entity screen add dummy data for now

--- core/src/main/java/com/csi4999/screens/SavedEntitiesScreen.java
+++ core/src/main/java/com/csi4999/screens/SavedEntitiesScreen.java
@@ -5,11 +5,11 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.viewport.FitViewport;
@@ -21,6 +21,12 @@ public class SavedEntitiesScreen implements Screen {
     private final FitViewport savedEntitiesViewport;
     private final ALifeApp app;
     private Stage stage;
+
+    private final Boolean noDatabase = true;
+
+    private String[] creatureNames;
+    private  String[] envNames;
+
     public SavedEntitiesScreen(ALifeApp app) {
         this.app = app;
 
@@ -34,39 +40,130 @@ public class SavedEntitiesScreen implements Screen {
 
         stage = new Stage(savedEntitiesViewport, app.batch);
         Gdx.input.setInputProcessor(stage);
+
+        if (noDatabase) {
+            creatureNames = new String[50];
+            envNames = new String[50];
+            for (int i = 0; i < 50; i ++) {
+                creatureNames[i] = "Creature " + i;
+                envNames[i] = "Environment " + i;
+            }
+
+        }
     }
     @Override
     public void show() {
+        // main table for the screen
         Table mainTable = new Table();
         mainTable.setFillParent(true);
-        mainTable.top();
-        mainTable.align(Align.center);
+        mainTable.top().left();
+//        mainTable.align(Align.left);
+//        mainTable.setDebug(true);
+        Table rightSide = new Table();
 
+        Table creatures = new Table();
+        for (String creature : creatureNames){
+            TextButton creatureButton = new TextButton(creature, skin);
+            creatureButton.addListener(new ClickListener(){
+                @Override
+                public void clicked(InputEvent event, float x, float y) {
+                    rightSide.clearChildren();
+                    Label creatureName = new Label(creature, skin);
+                    rightSide.add(creatureName).fill();
+                }
+            });
+            creatures.add(creatureButton).expand().fillX();
+            creatures.row();
+        }
+
+        Table environments = new Table();
+        for (String environment : envNames){
+            TextButton envButton = new TextButton(environment, skin);
+            envButton.addListener(new ClickListener(){
+                @Override
+                public void clicked(InputEvent event, float x, float y) {
+                    rightSide.clearChildren();
+                    Label envName = new Label(environment, skin);
+                    rightSide.add(envName).fill();
+                }
+            });
+            environments.add(envButton).expand().fillX();
+            environments.row();
+        }
+
+        ScrollPane creatureList = new ScrollPane(creatures, skin);
+        creatureList.setScrollBarPositions(true, false);
+
+        ScrollPane envList = new ScrollPane(environments, skin);
+        envList.setScrollBarPositions(true, false);
+
+        Stack leftSide = new Stack();
+        leftSide.addActor(creatureList);
+        leftSide.addActor(envList);
+
+
+
+//        SplitPane test = new SplitPane(leftSide, new TextButton("jkdslhfkdjshf", skin), false, skin);
+        SplitPane test = new SplitPane(leftSide, rightSide, false, skin);
 
         TextButton backButton = new TextButton("Go Back", skin);
-        backButton.setColor(1f, 0f, 0f, 1f);
-        TextButton option2 = new TextButton("Placeholder", skin);
-
-
         backButton.addListener(new ClickListener(){
-            @Override
+                        @Override
             public void clicked(InputEvent event, float x, float y) {
                 ((Game)Gdx.app.getApplicationListener()).setScreen(new MainMenuScreen(app));
             }
         });
+        TextButton viewCreatures = new TextButton("Saved Creatures", skin);
+        TextButton viewEnvironments = new TextButton("Saved Environments", skin);
+        mainTable.add(viewCreatures).fill().uniform();
+        mainTable.add(viewEnvironments).fill().uniform();
+        mainTable.row();
+        mainTable.center();
+        mainTable.add(test).fill().colspan(2);
+        mainTable.row();
+        mainTable.add(backButton).fill().colspan(2);
 
-        option2.addListener(new ClickListener(){
+        ChangeListener tab_listener = new ChangeListener() {
             @Override
-            public void clicked(InputEvent event, float x, float y) {
-                ((Game)Gdx.app.getApplicationListener()).setScreen(new SimScreen(app));
+            public void changed(ChangeEvent event, Actor actor) {
+                creatureList.setVisible(viewCreatures.isChecked());
+                envList.setVisible(viewEnvironments.isChecked());
             }
-        });
+        };
+        viewCreatures.addListener(tab_listener);
+        viewEnvironments.addListener(tab_listener);
+
+        ButtonGroup tabs = new ButtonGroup();
+        tabs.setMinCheckCount(1);
+        tabs.setMaxCheckCount(1);
+        tabs.add(viewCreatures);
+        tabs.add(viewEnvironments);
 
 
-        mainTable.row().pad(0, 0, 50, 0);
-        mainTable.add(option2).fill().uniform();
-        mainTable.row().pad(0, 0, 10, 0);
-        mainTable.add(backButton).fill().uniform();
+//        TextButton backButton = new TextButton("Go Back", skin);
+//        backButton.setColor(1f, 0f, 0f, 1f);
+//        TextButton option2 = new TextButton("Placeholder", skin);
+//
+//
+//        backButton.addListener(new ClickListener(){
+//            @Override
+//            public void clicked(InputEvent event, float x, float y) {
+//                ((Game)Gdx.app.getApplicationListener()).setScreen(new MainMenuScreen(app));
+//            }
+//        });
+//
+//        option2.addListener(new ClickListener(){
+//            @Override
+//            public void clicked(InputEvent event, float x, float y) {
+//                ((Game)Gdx.app.getApplicationListener()).setScreen(new SimScreen(app));
+//            }
+//        });
+//
+//
+//        mainTable.row().pad(0, 0, 50, 0);
+//        mainTable.add(option2).fill().uniform();
+//        mainTable.row().pad(0, 0, 10, 0);
+//        mainTable.add(backButton).fill().uniform();
 
         stage.addActor(mainTable);
     }
@@ -76,13 +173,22 @@ public class SavedEntitiesScreen implements Screen {
         Gdx.gl.glClearColor(.1f, .12f, .16f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+//        app.batch.setProjectionMatrix(savedEntitiesCam.combined);
+//        app.batch.begin();
+//        app.shapeDrawer.setColor(.18f, .2f, .28f, 1);
+//        app.shapeDrawer.line(savedEntitiesViewport.getWorldWidth() / 2, 0, savedEntitiesViewport.getWorldWidth() / 2, savedEntitiesViewport.getWorldHeight(), 2);
+//
+//        app.batch.end();
+
         stage.act();
         stage.draw();
     }
 
     @Override
     public void resize(int width, int height) {
+
         stage.getViewport().update(width, height, true);
+        savedEntitiesCam.update();
     }
 
     @Override
