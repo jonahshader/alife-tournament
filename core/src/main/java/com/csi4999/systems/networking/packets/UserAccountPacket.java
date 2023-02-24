41d35473b00fca5c53c7ce1f8531c3aa02f06207
rfrost114 <82419789+rfrost114@users.noreply.github.com>
2023-02-24 14:22:08 -0500

    implement user account packet

---
+++ core/src/main/java/com/csi4999/systems/networking/packets/UserAccountPacket.java
@@ -0,0 +1,27 @@
+package com.csi4999.systems.networking.packets;
+
+import com.csi4999.systems.creature.SensorBuilder;
+import com.csi4999.systems.creature.ToolBuilder;
+import java.util.List;
+
+public class UserAccountPacket {
+    public long userID;
+    public List<ToolBuilder> toolBuilders;
+    public List<SensorBuilder> sensorBuilders;
+
+    public long numberOfTools;
+    public long numberOfSensors;
+
+    public  UserAccountPacket(){} // empty constructor for Kryo
+
+    public  UserAccountPacket(long userID, List<ToolBuilder> toolBuilders, List<SensorBuilder> sensorBuilders, long numberOfTools, long numberOfSensors) {
+        this.userID = userID;
+        this.toolBuilders = toolBuilders;
+        this.sensorBuilders = sensorBuilders;
+
+        // are these just the length of the list ?
+        this.numberOfTools = numberOfTools;
+        this.numberOfSensors = numberOfSensors;
+    }
+
+}
